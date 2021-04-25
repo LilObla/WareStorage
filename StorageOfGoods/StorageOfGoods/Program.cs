8bb7e64f71d7f50de725e92153eb185da3a4aa49
Animeboy <39490975+LilObla@users.noreply.github.com>
2021-04-25 18:52:08 +0900

    Add files via upload

--- StorageOfGoods/StorageOfGoods/Program.cs
+++ StorageOfGoods/StorageOfGoods/Program.cs
@@ -19,32 +19,4 @@ namespace Storage
             TLostGoodsCount.Start();
         }
     }
-
-    public class Goods : IEquatable<Goods>
-    {
-        public string NameOfGoods { get; set; }
-
-        public int SizeOfGoods { get; set; }
-
-        public override string ToString()
-        {
-            return "Name: " + NameOfGoods + "    Volume: " + SizeOfGoods;
-        }
-        public override bool Equals(object obj)
-        {
-            if (obj == null) return false;
-            Goods objAsPart = obj as Goods;
-            if (objAsPart == null) return false;
-            else return Equals(objAsPart);
-        }
-        public int SizeOfLostGoods()
-        {
-            return SizeOfGoods;
-        }
-        public bool Equals(Goods other)
-        {
-            if (other == null) return false;
-            return (this.NameOfGoods.Equals(other.NameOfGoods));
-        }
-    }
 }
